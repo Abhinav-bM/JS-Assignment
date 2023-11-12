00a61041d5c935b64c3efe93dd5d681a16dfd64b
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-12 21:10:41 +0530

    update

--- 11-eleven.js
+++ 11-eleven.js
@@ -1,2 +1,13 @@
 // Write a function to calculate the factorial of a given number n
 
+function factOfNum (number){
+    
+    let fact = 1;
+    for ( let i = 1; i <= number; i++){
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+console.log(factOfNum(5));
