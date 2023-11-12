794c68ad1976ab0a666cb7ee284cd21b8f86da29
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-12 12:45:36 +0530

    update

--- 5-five.js
+++ 5-five.js
@@ -1,12 +1,17 @@
 // Write a function to check if a given number is prime
 
-function primeCheck(num){
-    if(num % 2 === 0){
-        return "prime";
-    }
-    else{
-        return "not-prime";
-    }
-}
 
-console.log(primeCheck(3));
+function primeCheck (num){
+    if (num < 2){
+        return "not prime";
+    }
+    
+    for (let i = 2; i <= Math.sqrt(num); i++)
+
+    if(num % i === 0) return "not prime";
+    
+    return "prime";
+    
+    
+}
+console.log(primeCheck(6));
--- 9-nine.js
+++ 9-nine.js
@@ -1,1 +1,2 @@
 // Write a function to generate all prime numbers up to a given limit
+
