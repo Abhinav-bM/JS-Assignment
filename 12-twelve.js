4306c43e6334392d9aae0240617a945fdcf8c47c
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-16 14:24:37 +0530

    update

--- 12-twelve.js
+++ 12-twelve.js
@@ -1,20 +1,34 @@
 // Write a function to check given string is palindrome or not
 
-function checkPalindrome (string){
+// function checkPalindrome (string){
 
-    let reversed = '';
-    for (let i = string.length-1; i >= 0; i--){
-        reversed = reversed + string[i]
-    }
+//     let reversed = '';
+//     for (let i = string.length-1; i >= 0; i--){
+//         reversed = reversed + string[i]
+//     }
 
-    if(string === reversed){
-        console.log(`It is a palindrome`);
+//     if(string === reversed){
+//         console.log(`It is a palindrome`);
+//     }
+//     else{
+//         console.log(`It is not a palindrome`);
+//     }
+// }
+
+// let check = `madam`;
+
+// checkPalindrome(check);
+
+
+// using methods
+function palindromeCheck(str){
+    let strReversed = str.split('').reverse().join('');
+    if(strReversed === str){
+        return "it is a palindrome";
     }
     else{
-        console.log(`It is not a palindrome`);
+        return "it is not a palindrome";
     }
 }
 
-let check = `madam`;
-
-checkPalindrome(check);
+console.log(palindromeCheck("mada"));
